bb1abc7411b9d58ec92d0f1bd471a11b8e9cf880
Renan Araujo <araujo2kg@gmail.com>
2024-11-27 19:42:40 -0300

    Create option to choose sketch grid size
    - Add function to get the grid size from user
    - Add function to delete current sketch grid
    - Add button to create a new sketch grid of
    different sizes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let size = 30;
-const container = document.querySelector(".container");
 
 function createSketchGrid(size) {
+    const container = document.querySelector(".container");
     for (let i = 0; i < size * size; i++) {
         let square = document.createElement("div");
         square.classList.add("squares");
@@ -14,5 +14,27 @@ function createSketchGrid(size) {
 function applyColor(event) {
     event.target.style.backgroundColor = "black";
 }
+
+function setGridSize() {
+    let sizeInput;
+    do {
+        sizeInput = Math.round(+prompt("Choose a grid size (100 Max):"));
+    } while (!(sizeInput > 0 && sizeInput < 101));
+    size = sizeInput;
+}
+
+function deleteSketchGrid() {
+    const squares = document.querySelectorAll(".squares");
+    for (let square of squares) {
+        square.remove();
+    }
+}
+
+const setSizeButton = document.querySelector("#setSize");
+setSizeButton.addEventListener("click", () => {
+    setGridSize();
+    deleteSketchGrid();
+    createSketchGrid(size);
+});
 
 createSketchGrid(size);
