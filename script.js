bb31a1781418a26cf8dfa08cf4214ce4871dc5d6
Renan Araujo <araujo2kg@gmail.com>
2024-11-27 18:07:53 -0300

    Improve basic structure
    - Add event listener to "draw" in the sketch container
    - Separate into functions

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-let size = 20;
+let size = 30;
 const container = document.querySelector(".container");
 
-for (let i = 0; i < size * size; i++) {
-    let square = document.createElement("div");
-    square.classList.add("squares");
-    container.appendChild(square);
+function createSketchGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+        let square = document.createElement("div");
+        square.classList.add("squares");
+        square.style.width = `calc(100% / ${size})`;
+        square.addEventListener("mouseenter", applyColor);
+        container.appendChild(square);
+    }
 }
 
-const squares = document.querySelectorAll(".squares");
-squares.forEach((square) => {
-    square.style.width = `calc(100% / ${size})`;
-});
+function applyColor(event) {
+    event.target.style.backgroundColor = "black";
+}
+
+createSketchGrid(size);
